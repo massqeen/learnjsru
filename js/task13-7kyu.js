e5c64362a1315bbbc6f4cf04da6799e4099b72c9
Arthur Plyuschevsky <62159695+massqeen@users.noreply.github.com>
2020-07-25 12:22:25 +0300

    added 7kyu task - not solved

---
+++ js/task13-7kyu.js
@@ -0,0 +1,22 @@
+/*In this Kata, you will be given two positive integers a and b and your task will be to apply the following operations:
+
+i) If a = 0 or b = 0, return [a,b]. Otherwise, go to step (ii);
+
+ii) If a ≥ 2*b, set a = a - 2*b, and repeat step (i). Otherwise, go to step (iii);
+iii) If b ≥ 2*a, set b = b - 2*a, and repeat step (i). Otherwise, return [a,b].*/
+function solve(a, b) {
+  if (a === 0 || b === 0) {
+    return [a, b];
+  }
+  if (a >= 2 * b) {
+    a = a - 2 * b;
+    return solve(a, b);
+  }
+  if (b >= 2 * a) {
+    b = b - 2 * a;
+    return solve(a, b);
+  }
+  return [a, b];
+}
+console.log(solve(100000000000, 3));
+let a;
