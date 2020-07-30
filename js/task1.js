f836f0725aae964ce8851574934c8903bddf6693
massqeen <massqeen@gmail.com>
2020-07-31 02:17:32 +0300

    added task 1

--- js/task1.js
+++ js/task1.js
@@ -1,13 +1,45 @@
-/*Напишите код, выполнив задание из каждого пункта отдельной строкой:
+/*  Класс Clock написан в функциональном стиле.
+Перепишите его, используя современный синтаксис классов.*/
+class Clock {
+  constructor({ template }) {
+    this.template = template;
+  }
 
-Создайте пустой объект user.
-Добавьте свойство name со значением John.
-Добавьте свойство surname со значением Smith.
-Измените значение свойства name на Pete.
-Удалите свойство name из объекта.*/
-const user = {};
-user.name = 'John';
-user.surname = 'Smith';
-user.name = 'Pete';
-delete user.name;
-console.log(user);
+  render() {
+    let date = new Date();
+
+    let hours = date.getHours();
+    if (hours < 10) {
+      hours = '0' + hours;
+    }
+
+    let mins = date.getMinutes();
+    if (mins < 10) {
+      mins = '0' + mins;
+    }
+
+    let secs = date.getSeconds();
+    if (secs < 10) {
+      secs = '0' + secs;
+    }
+
+    let output = this.template
+      .replace('h', hours)
+      .replace('m', mins)
+      .replace('s', secs);
+
+    console.log(output);
+  }
+
+  stop() {
+    clearInterval(this.timer);
+  }
+
+  start() {
+    this.render();
+    this.timer = setInterval(() => this.render(), 1000);
+  }
+}
+
+let clock = new Clock({ template: 'h:m:s' });
+clock.start();
