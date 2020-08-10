7acb40d026b88c48f6216252a5f70cb135500dd4
massqeen <62159695+massqeen@users.noreply.github.com>
2020-08-10 22:03:52 +0300

    added solved task

--- js/task17-6kyu.js
+++ js/task17-6kyu.js
@@ -6,36 +6,32 @@ solve(100) = 99. Digit Sum for 99 = 9 + 9 = 18. No other number <= 100 has a hig
 solve(10) = 9
 solve(48) = 48. Note that 39 is also an option, but 48 is larger.
 Input range is 0 < n < 1e11*/
-const solve = (n) => {
-  const sumOfDigits = (number) => {
-    return String(number)
-      .split('')
-      .reduce((acc, item) => {
-        return acc + +item;
-      }, 0);
-  };
-  let max = sumOfDigits(n);
-  let b = 1;
-  result = n;
-  let current = n;
-  while (String(current).length > String(n).length - 2) {
-    // let current = (n - 1) * b + (b - 1);
-    current -= 1;
-    let currentSumOfDigits = sumOfDigits(current);
-    if (
-      currentSumOfDigits > max ||
-      (currentSumOfDigits === max && current > result)
-    ) {
-      max = currentSumOfDigits;
-      result = current;
-    }
-    // n /= 10;
-    // b *= 10;
-  }
-  return result;
-};
 
-console.log(solve(92063948914));
+const solve = (n) => {
+  const array = [...('' + n)];
+  const sumDigits = (arr) => arr.reduce((acc, item) => +acc + +item);
+  const findLargestInteger = (arr) => +arr.reduce((acc, item) => acc + item);
+  if (array.length === 1) {
+    return n;
+  }
+  let i = 1;
+  while (array[i] === '9') {
+    i += 1;
+  }
+  const arrayModified = [...array];
+  arrayModified[i - 1] -= 1;
+  for (let j = i; j < arrayModified.length; j += 1) {
+    arrayModified[j] = '9';
+  }
+  if (!arrayModified[0]) {
+    arrayModified.splice(0, 1);
+  }
+  const initialSum = sumDigits(array);
+  const currentSum = sumDigits(arrayModified);
+  const largestInteger = findLargestInteger(arrayModified);
+  return initialSum === currentSum ? n : largestInteger;
+};
+console.log(solve(198));
 //79320 - 78999
 //29652 - 28999
 //99737 - 98999
@@ -45,10 +41,13 @@ console.log(solve(92063948914));
 //92063948914 - 89999999999
 //11550034505 - 9999999999
 //14997365686 - 9999999999
-// let n = 92063948914;
-// const regexp = /[^8-9][9]+/;
-// let copyN = n - 1;
-// while (!regexp.test(String(copyN))) {
-//   copyN -= 1;
-// }
-// console.log(regexp.test('69999999999'));
+/*BEST PRACTICE
+ var sum=(x)=>[...x].reduce((a,b)=>+a + +b)
+function solve(n){
+    let s = [...''+n], i=1
+    while (i<s.length && s[i]==9) i++
+    s[i-1] -= 1
+    let num = s.slice(0,i).join``+'9'.repeat(s.slice(i).length)
+    return sum(num)>sum(''+n)?+num:n
+}
+*/
